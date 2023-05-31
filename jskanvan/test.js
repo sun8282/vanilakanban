6463ffc5f2410294f4dd4facf94228a21f12aeb0
sun8282 <109707901+sun8282@users.noreply.github.com>
2023-05-31 11:21:39 +0900

    Add files via upload

--- jskanvan/test.js
+++ jskanvan/test.js
@@ -3,7 +3,18 @@ const addbox = document.querySelector(".addbox");
 const closebtn = [...document.querySelectorAll(".close")];
 const itembtn = [...document.querySelectorAll(".additem")];
 const addbtn = [...document.querySelectorAll(".addbtn")];
+const addinput = document.querySelector(".addinput");
+const itemcontainer = document.querySelector(".itemcontainer");
+const draggables = document.querySelectorAll(".draggable");
+draggables.forEach((draggable) => {
+  draggable.addEventListener("dragstart", () => {
+    draggable.classList.add("dragging");
+  });
 
+  draggable.addEventListener("dragend", () => {
+    draggable.classList.remove("dragging");
+  });
+});
 containers.forEach((container) => {
   container.addEventListener("dragover", (e) => {
     e.preventDefault();
@@ -43,24 +54,66 @@ const addcontainer = (value) => {
   item.className = "container";
   item.setAttribute("draggable", true);
   item.innerHTML = `
-    <h3>${value}</h3>
-    <ul class ="ul">
-        
-    </ul>
-    <ul>
-
-    </ul>
-    <button class="additem">add</button>
-    <div class="addbox none">
-              <input type="text" class="iteminput">
-              <div class="btns">
-              <button class="addbtn">add</button>
-              <span class="close">❌</span>
-              </div>
+  <h3> ${value}</h3>
+  <button class="additem">add</button>
+  <div class="addbox none">
+            <input type="text" class="iteminput">
+            <div class="btns">
+            <button class="addbtn">add</button>
+            <span class="close">❌</span>
+            </div>
+  </div>
     `;
 
   itemcontainer.append(item);
-  console.log(article);
+  const itembtn = [...document.querySelectorAll(".additem")];
+  const containers = document.querySelectorAll(".container");
+  const closebtn = [...document.querySelectorAll(".close")];
+  const addbtn = [...document.querySelectorAll(".addbtn")];
+  const addinput = document.querySelector(".addinput");
+  itembtn.forEach((e) => {
+    e.addEventListener("click", (el) => {
+      console.log(el.target.nextSibling.nextSibling);
+
+      el.target.nextSibling.nextSibling.classList.remove("none");
+    });
+  });
+  closebtn.forEach((e) => {
+    e.addEventListener("click", (el) => {
+      console.log(el.target.parentNode.parentNode);
+      el.target.parentNode.parentNode.classList.add("none");
+    });
+  });
+  containers.forEach((container) => {
+    container.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      const afterElement = getDragAfterElement(container, e.clientX);
+      const draggable = document.querySelector(".dragging");
+      if (afterElement === undefined) {
+        container.appendChild(draggable);
+      } else {
+        container.insertBefore(draggable, afterElement);
+      }
+    });
+    addbtn.forEach((e) => {
+      e.addEventListener("click", (el) => {
+        console.log(el.target.parentNode.previousSibling.previousSibling);
+        const parentel = el.target.parentNode.parentNode.parentNode;
+        console.log(parentel.children[0]);
+        const input = el.target.parentNode.previousSibling.previousSibling;
+        if (
+          input.value !== "" &&
+          parentel.children[0].className == "addcontainer"
+        ) {
+          addcontainer(addinput.value);
+          addinput.value = "";
+        } else if (input.value !== "" && parentel.className == "container") {
+          additem(input.value, parentel);
+          input.value = "";
+        }
+      });
+    });
+  });
 };
 const additem = (value, el) => {
   console.log(el);
@@ -72,6 +125,7 @@ const additem = (value, el) => {
   item.innerText = value;
   el.append(item);
   const draggables = document.querySelectorAll(".draggable");
+
   draggables.forEach((draggable) => {
     draggable.addEventListener("dragstart", () => {
       draggable.classList.add("dragging");
@@ -86,9 +140,12 @@ addbtn.forEach((e) => {
   e.addEventListener("click", (el) => {
     console.log(el.target.parentNode.previousSibling.previousSibling);
     const parentel = el.target.parentNode.parentNode.parentNode;
-    console.log(parentel);
+    console.log(parentel.children[0]);
     const input = el.target.parentNode.previousSibling.previousSibling;
-    if (input.value !== "" && parentel.className == "addcontainer") {
+    if (
+      input.value !== "" &&
+      parentel.children[0].className == "addcontainer"
+    ) {
       addcontainer(addinput.value);
       addinput.value = "";
     } else if (input.value !== "" && parentel.className == "container") {
